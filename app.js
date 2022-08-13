e181e67057918e7211ac83a6f88d4342f5a22cc7
GuiOliver <guilherme_oliveira27@outlook.com>
2022-08-13 01:15:50 -0300

    New features to the Hex Color and RGB

--- app.js
+++ app.js
@@ -1,5 +1,9 @@
 const hexInput = document.getElementById('hexInput');
 const inputColor = document.getElementById('inputColor');
+const alteredColor = document.getElementById('alteredColor');
+const alteredColorText = document.getElementById('alteredColorText');
+const slider = document.getElementById('slider');
+const sliderText = document.getElementById('sliderText');
 
 hexInput.addEventListener('keyup', () => {
     const hex = hexInput.value;
@@ -16,3 +20,61 @@ const isValidHex = (hex) => {
     const strippedHex = hex.replace('#', '');
     return strippedHex.length === 3 || strippedHex.length === 6;
 }
+
+const convertHexToRGB = (hex) => {
+    if(!isValidHex(hex)) return null;
+
+    let strippedHex = hex.replace('#', '');
+
+    if(strippedHex.length === 3) {
+        strippedHex = strippedHex[0] + strippedHex
+        + strippedHex[1] + strippedHex[1]
+        + strippedHex[2] + strippedHex[2]
+    }
+    
+    const r = parseInt(strippedHex.substring(0,2), 16);
+    const g = parseInt(strippedHex.substring(2, 4), 16);
+    const b = parseInt(strippedHex.substring(4,6), 16);
+
+    return {r,g,b}
+}
+
+const converRGBToHex = (r,g,b) => {
+    const firstPair = ("0" + r.toString(16)).slice(-2);
+    const secondtPair = ("0" + g.toString(16)).slice(-2);
+    const thirdPair = ("0" + b.toString(16)).slice(-2);
+
+    const hex = "#"  + firstPair + secondtPair +  thirdPair;
+    return hex;
+}
+
+const alterColor = (hex, porcentage) => {
+    const {r,g,b} = convertHexToRGB(hex);
+
+    const amount = Math.floor((porcentage/100) * 255);
+
+    const newR = increaseWithin0To255(r, amount);
+    const newG = increaseWithin0To255(g, amount);
+    const newB = increaseWithin0To255(b, amount)
+    return converRGBToHex(newR, newG, newB);
+}
+
+const increaseWithin0To255 = (hex, amount) => {
+    // const newHex = hex + amount;
+    // if(newHex > 255) return 255;
+    // if(newHex < 0) return 0;
+    // return newHex;
+    return Math.min(255, Math.max(0, hex + amount));
+}
+
+alterColor("000", -10)
+
+slider.addEventListener('input', () => {
+    if(!isValidHex(hexInput.value)) return;
+
+    sliderText.textContent = `${slider.value}%`;
+
+    const alteredHex = alterColor(hexInput.value, slider.value);
+    alteredColor.style.backgroundColor = alteredHex;
+    alteredColorText.innerText = `Altered Color ${alteredHex}`;
+})
